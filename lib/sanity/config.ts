d0ca4be8019d40cb446ad339b4df498d5767365f
videovulture <30363299+videovulture@users.noreply.github.com>
2024-03-01 16:19:44 -0800

    main 6

--- lib/sanity/config.ts
+++ lib/sanity/config.ts
@@ -21,6 +21,6 @@ export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2023-03-25";
 
 // This is the document id used for the preview secret that's stored in your dataset.
-// The secret protects against unauthorized access to your draft content and have a lifetime of 60 minutes, to protect against bruteforcing.
+// The secret protects against unauthorized access to your draft content and have a lifetime of 60 minutes, to protect against brute forcing.
 export const previewSecretId = process.env
   .SANITY_REVALIDATE_SECRET as string;
