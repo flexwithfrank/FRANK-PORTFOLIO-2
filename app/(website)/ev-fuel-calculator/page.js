bc54e03624b7205fafa278017ef043707eaa980d
videovulture <30363299+videovulture@users.noreply.github.com>
2024-05-14 17:16:14 -0700

    main-900

---
+++ app/(website)/ev-fuel-calculator/page.js
@@ -0,0 +1,7 @@
+import Calculator from "./calculator";
+
+export default async function CalculatorPage() {
+  return <Calculator />;
+}
+
+// export const revalidate = 60;
--- components/ProjectGrid.js
+++ components/ProjectGrid.js
@@ -355,6 +355,51 @@ export default function ProjectGrid() {
           </Link>
         </motion.div>
         {/* grid */}
+          {/* grid */}
+          <motion.div
+          initial={{ y: 50, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{
+            duration: 0.5,
+            ease: "easeOut",
+            delay: 0.45
+          }}
+          viewport={{ once: true }}
+          className="group relative h-full">
+          <a
+            className=" flex h-full flex-col justify-between space-y-32 rounded-3xl bg-[#f2f4f7] p-8" target="_blank"
+            href="https://ev-fuel-calculator.pages.dev/">
+            <div class="align-center mx-auto items-center justify-center overflow-hidden rounded-2xl bg-gray-100 pt-20 transition-transform duration-500 ease-in-out group-hover:scale-105">
+              <img
+                class="mx-auto rounded-2xl object-cover px-12 mx-w-xl"
+                src="https://d15k2d11r6t6rl.cloudfront.net/pub/bfra/81p7eutj/zfd/mix/9cw/download%20%281%29.png"
+                alt="Image Description"
+              />
+            </div>
+
+            <div>
+              <h3 class="inter-medium relative inline-block pb-2 text-2xl font-bold tracking-tight text-black before:absolute before:bottom-0.5 before:start-0 before:-z-[1] before:h-1 before:w-full before:origin-left before:scale-x-0 before:transition group-hover:before:scale-x-100 dark:text-white">
+                EV Fuel Calculator
+              </h3>
+              <div class="mt-3 flex flex-wrap gap-2">
+                <span class="inter-medium cursor-pointer rounded-full border border-black px-3 py-1.5 text-[12px] text-black transition duration-150 hover:bg-black hover:text-white dark:border-neutral-700 dark:bg-neutral-900 dark:text-neutral-400 sm:text-xs">
+                  HTML
+                </span>
+                <span class="inter-medium inter-medium cursor-pointer rounded-full border border-black px-3 py-1.5 text-[12px] text-black transition duration-150 hover:bg-black hover:text-white dark:border-neutral-700 dark:bg-neutral-900 dark:text-neutral-400 sm:text-xs">
+                  CSS
+                </span>
+              
+                <span class="inter-medium inter-medium cursor-pointer rounded-full border border-black px-3 py-1.5 text-[12px] text-black transition duration-150 hover:bg-black hover:text-white dark:border-neutral-700 dark:bg-neutral-900 dark:text-neutral-400 sm:text-xs">
+                  AMPScript
+                </span>
+                <span class="inter-medium inter-medium cursor-pointer rounded-full border border-black px-3 py-1.5 text-[12px] text-black transition duration-150 hover:bg-black hover:text-white dark:border-neutral-700 dark:bg-neutral-900 dark:text-neutral-400 sm:text-xs">
+                  Email
+                </span>
+              </div>
+            </div>
+          </a>
+        </motion.div>
+        {/* grid */}
 
         {/* end */}
       </div>
